15dce0ef030c21e79cc0622e3daf3d35af412644
Denis <denislimarev@gmail.com>
2022-08-28 22:22:48 +0300

    update rule formatting

--- testdata/src/errCheckInIf/errCheckInIf.go
+++ testdata/src/errCheckInIf/errCheckInIf.go
@@ -6,13 +6,20 @@ func (foo) boo() error {
 	return nil
 }
 
+func (foo) boo2() (string, error) {
+	return "", nil
+}
+
 func boo() error {
 	return nil
 }
 
+func boo2() (string, error) {
+	return "", nil
+}
+
 func warning1() {
 	var err2 error
-
 	if err := boo(); err2 != nil { // want `errCheckInIf: returned error 'err' must be checked`
 		print(err)
 	}
@@ -34,10 +41,29 @@ func warning2() {
 		print(err)
 	}
 
+	if _, err := d.boo2(); err2 != nil { // want `errCheckInIf: returned error 'err' must be checked`
+		print(err)
+	}
+
 	var err error
 
 	if err = d.boo(); err2 != nil { // want `errCheckInIf: returned error 'err' must be checked`
 	}
 
+	if _, err = d.boo2(); err2 != nil { // want `errCheckInIf: returned error 'err' must be checked`
+	}
+
 	print(err)
+}
+
+func warning3() {
+	var (
+		err2 error
+	)
+	if _, err := boo2(); err2 != nil { // want `errCheckInIf: returned error 'err' must be checked`
+		print(err)
+	}
+	if _, err := boo2(); err2 != nil { // want `errCheckInIf: returned error 'err' must be checked`
+		print(err)
+	}
 }
